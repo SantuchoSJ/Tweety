f751f5999721017b0ec41d3f9422281e51b1eacb
SantuchoSJ <santiagojoaquinsantucho@gmail.com>
2021-01-21 13:08:10 -0300

    WS terminado sin socket

--- app.js
+++ app.js
@@ -2,28 +2,38 @@ const express = require( 'express' );
 const morgan = require('morgan'); //middleware application logger
 const nunjucks = require( 'nunjucks' );
 const routes = require('./routes');
-const app = express(); // crea una instancia de una aplicación de express
 const bodyParser= require('body-parser')
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-var socketio = require('socket.io');//Como usar el socketio?
+const socketio = require('socket.io');
+
+const app = express(); // crea una instancia de una aplicación de express
+
 // Configurando Nunjucks
 app.set('view engine', 'html'); // hace que res.render funcione con archivos html
 app.engine('html', nunjucks.render); // cuando le den archivos html a res.render, va a usar nunjucks
 nunjucks.configure('views'); // apunta a nunjucks al directorio correcto para los templates
 
-app.use(morgan('tiny'))
-app.use('/', routes);
 
-
-
+//STATIC
 app.use(express.static('./public'))
 
+//MORGAN
+app.use(morgan('tiny'))
 
+//Body-parser
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+
+
+//Server
 
 
 app.listen(3000, function(){
     console.log('Estas escuhando en el puerto 3000')
 });
-/*var server = app.listen(3000);
-var io = socketio.listen(server);*/
+app.use('/', routes);
+
+
+
+
+
